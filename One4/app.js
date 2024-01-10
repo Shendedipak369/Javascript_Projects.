9b1eed595aa4b4a77976061ed9b74a5994c60ebc
Dipak Shende <136945342+Shendedipak369@users.noreply.github.com>
2024-01-10 18:24:08 +0530

    Weather-App
    The weather app is a user-friendly and visually appealing application that provides accurate and localized weather information. Users can easily access current weather conditions and forecasts for different locations. With real-time updates  for severe weather conditions, it ensures users stay informed. The interface is designed for simplicity and accessibility.

---
+++ One4/app.js
@@ -0,0 +1,57 @@
+const apiKey = "1a22b50da5376fe6dadb6b92d3d1e9aa";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if (data.weather[0].main == "cloud") {
+            weatherIcon.src = "clouds.png";
+
+        } else if (data.weather[0].main == "clear") {
+            weatherIcon.src = "clear.png";
+
+        } else if (data.weather[0].main == "rain") {
+            weatherIcon.src = "rain.png";
+
+        } else if (data.weather[0].main == "mist") {
+            weatherIcon.src = "mist.png";
+
+        } else if (data.weather[0].main == "snow") {
+            weatherIcon.src = "snow.png";
+
+        } else if (data.weather[0].main == "drizzle") {
+            weatherIcon.src = "drizzle.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+         document.querySelector(".error").style.display = "none";
+
+
+
+
+    }
+
+
+
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+
